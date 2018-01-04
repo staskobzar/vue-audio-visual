c9ce6eefc2562a1614fe7953f03b4dd2e3f71eed
Stas Kobzar <stas@modulis.ca>
2018-01-03 19:58:30 -0500

    Webpack build

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,14 +22,10 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/index.js'
+    'vue-audio-visual': './src/index.js'
   },
   output: {
-    path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
