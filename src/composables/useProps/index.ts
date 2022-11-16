54214435beaa43aaa5836925c3ca850b51d66b8c
Stas Kobzar <stas.kobzar@clearlyip.com>
2022-11-15 21:21:51 -0500

    add types to build

--- src/composables/useProps/index.ts
+++ src/composables/useProps/index.ts
@@ -4,9 +4,6 @@ export * from './Line'
 export * from './Circle'
 export * from './Waveform'
 export * from './Media'
-import { commonProps } from './common'
-
-export const PropsCommon = commonProps
 
 export interface Props {
   canvWidth: number
